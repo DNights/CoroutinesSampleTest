f67833f81c5b8a32d056fc9ba2e0c151c5896770
hyundonghan <guehgk456@gmail.com>
2019-08-26 22:24:39 +0900

    fix AndroidAsyncTask

--- app/src/main/java/com/dnights/coroutinessample/AndroidAsyncTask.kt
+++ app/src/main/java/com/dnights/coroutinessample/AndroidAsyncTask.kt
@@ -1,46 +1,45 @@
 package com.dnights.coroutinessample
 
-import android.annotation.SuppressLint
 import android.os.AsyncTask
 import android.widget.ProgressBar
 
-object AndroidAsyncTask {
-
+class AndroidAsyncTask(private val progressAsyncTask: ProgressBar) : AsyncTask<Unit, Int, Int>() {
     private val PROGRESS_MAX = 100
     private val PROGRESS_START = 0
     private val JOB_TIME = 4000 // ms
 
-    fun startAsyncTask(progressAsyncTask: ProgressBar) {
-        val loadData = @SuppressLint("StaticFieldLeak")
-        object : AsyncTask<Unit, Int, Int>() {
+    private var isCanceled = false
 
-            override fun doInBackground(vararg params: Unit?): Int {
-                for(i in PROGRESS_START..PROGRESS_MAX){
-                    Thread.sleep((JOB_TIME / PROGRESS_MAX).toLong())
-                    publishProgress(i)
-                }
-
-                return -1
-            }
-
-            override fun onProgressUpdate(vararg values: Int?) {
-                super.onProgressUpdate(*values)
-                progressAsyncTask.progress = values[0] ?: -1
-            }
-
-            override fun onPreExecute() {
-                super.onPreExecute()
-                // Show progress from UI thread
-                progressAsyncTask.progress = 0
-            }
-
-            override fun onPostExecute(result: Int?) {
-                super.onPostExecute(result)
-                // UI data update from UI thread
-                // Hide Progress from UI thread
-
-            }
+    override fun doInBackground(vararg params: Unit?): Int {
+        for(i in PROGRESS_START..PROGRESS_MAX){
+            if(isCanceled) return -1
+            Thread.sleep((JOB_TIME / PROGRESS_MAX).toLong())
+            publishProgress(i)
         }
-        loadData.execute(Unit)
+
+        return -1
+    }
+
+    override fun onProgressUpdate(vararg values: Int?) {
+        super.onProgressUpdate(*values)
+        progressAsyncTask.progress = values[0] ?: -1
+    }
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        // Show progress from UI thread
+        progressAsyncTask.progress = 0
+    }
+
+    override fun onPostExecute(result: Int?) {
+        super.onPostExecute(result)
+        // UI data update from UI thread
+        // Hide Progress from UI thread
+
+    }
+
+    override fun onCancelled() {
+        super.onCancelled()
+        isCanceled = true
     }
 }
